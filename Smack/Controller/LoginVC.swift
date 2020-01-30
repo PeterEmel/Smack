23e12167878c668a4b4f6c3308a533a4bd0b6214
Peter Emel <peteremel@peters-imac.local>
2020-01-29 16:43:55 -0800

    LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Peter Emel on 1/29/20.
+//  Copyright © 2020 Peter Emel. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    //Outlets
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
