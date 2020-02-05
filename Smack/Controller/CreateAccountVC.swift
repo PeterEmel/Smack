7adc62018eda92f6e9f3154b9247bd2ec5772b2c
Peter Emel <peteremel@peters-imac.local>
2020-02-05 05:43:35 -0800

    Create Use & Avatar setup

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -11,16 +11,29 @@ import UIKit
 class CreateAccountVC: UIViewController {
     
     //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
     
     @IBAction func createAccntPressed(_ sender: UIButton) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else {return}
         guard let email = emailTxt.text, emailTxt.text != "" else{return}
         guard let pass = passTxt.text, passTxt.text != "" else{return}
         
@@ -28,8 +41,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("Logged in user! ", AuthService.instance.authToken)
-                        //print("Logged in user!: \(AuthService.instance.authToken)")
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            
+                            if success {
+                                self.performSegue(withIdentifier: "unwindToChannel", sender: nil)
+                            }
+                        })
                     }else{
                         debugPrint("Error from login")
                     }
@@ -42,6 +59,7 @@ class CreateAccountVC: UIViewController {
    
     
     @IBAction func pickAvatarPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "toAvatarPicker", sender: nil)
     }
     @IBAction func pickBGColorPressed(_ sender: UIButton) {
     }
